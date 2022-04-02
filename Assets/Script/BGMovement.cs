b8ce367a772c83986fde673a7ee33c59fb58c29a
yulotomorrow <yulotomorrow@gmail.com>
2022-04-02 15:39:00 +0800

    Basic scene

---
+++ Assets/Script/BGMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMovement : MonoBehaviour
+{
+    public GameObject planet;
+
+    private GameObject bg;
+    private float center;
+    private const float centerX = 0f;
+    private const float rotateSpeed = 10f;
+    void Start()
+    {
+        center = planet.GetComponent<Transform>().position.y;
+        bg = gameObject;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A)) 
+        {
+            gameObject.transform.RotateAround(new Vector3(centerX, center, 0), Vector3.forward, -rotateSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D)) 
+        {
+            gameObject.transform.RotateAround(new Vector3(centerX, center, 0), Vector3.forward, rotateSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Script/CannonballShoot.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonballShoot : MonoBehaviour
+{
+	private const float cannonballSpeed = 20f;
+	private float pos;
+	private float posX;
+	private Vector3 originalPos;
+	void Start()
+	{
+		pos = gameObject.transform.position.y;
+		posX = gameObject.transform.position.x;
+		originalPos = gameObject.transform.position;
+	}
+
+	private bool isShoot = false;
+	void Update()
+	{
+		if (Input.GetKey(KeyCode.Space))
+		{
+			isShoot = true;
+		}
+		if (isShoot) 
+		{
+			pos += Time.deltaTime * cannonballSpeed;
+			gameObject.transform.position = new Vector3(posX, pos, 0);
+		}
+	}
+}
