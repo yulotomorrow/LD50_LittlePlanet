99d8df7b64d5f9c0121174e990c4f9c289a7e1a3
yulotomorrow <yulotomorrow@gmail.com>
2022-04-03 22:33:54 +0800

    Full game cycle

--- Assets/Script/ProgressionBar.cs
+++ Assets/Script/ProgressionBar.cs
@@ -2,22 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ProgressionBar : MonoBehaviour
 {
-    private const int limit = 30000;
+    private const int limit = 5000; // 10000 - 30000
     [SerializeField] private Slider progress;
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Canvas launch;
 
     private float prog = 0f;
     private int count = 0;
+    void Start()
+    {
+        count = 0;
+        prog = 0f;
+    }
     void FixedUpdate()
     {
         ++count;
-        prog = count / (1.0f * limit);
-        progress.value = prog;
+        if (count < limit)
+        {
+            prog = count / (1.0f * limit);
+            progress.value = prog;
+        }
+        else 
+        {
+            prog = 1f;
+            progress.value = prog;
+            launch.gameObject.SetActive(true);
+
+        }
+    }
+    private void Update()
+    {
+        if (prog == 1f && Input.GetKeyDown(KeyCode.F))
+        {
+            SceneManager.LoadScene("EndScene", LoadSceneMode.Single);
+        }
     }
 }
